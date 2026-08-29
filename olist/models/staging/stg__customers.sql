e35fb4768a15d9a4d04d9d9a666826c9a41767f5
jlsmith025 <jlsmith025@gmail.com>
2026-08-29 08:00:49 -0500

    Created dim_customer_order
    * Added codegen package
    * changed stg__customers to clarify `customer_unique_id`
    * finazlized `dim_order_customer`
    * created YML documentation and tests for `dim_order_customer`

--- olist/models/staging/stg__customers.sql
+++ olist/models/staging/stg__customers.sql
@@ -8,7 +8,7 @@ renamed as (
 
     select 
         customer_id as order_customer_id,
-        customer_unique_id customer_id,
+        customer_unique_id,
         customer_zip_code_prefix as customer_zip_code,
         customer_city,
         customer_state
