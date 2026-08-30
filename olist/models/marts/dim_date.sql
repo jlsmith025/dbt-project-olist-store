67e24d6d17a5b35233eca0d61507c4639fcf3b0d
jlsmith025 <jlsmith025@gmail.com>
2026-08-30 09:49:11 -0500

    Created fact_order_item
    * Expanded the date_spine range in `dim_date` to capture `shipping_limit_date` cases beyond the prior defined date range
    * Changed the join from products to product translation to outer and translated NULL English names to account for 2 product categories that do not have a translation in `product_category_name_translation`
    * Created new model `fact_order_item`
    * Created new `fact_order_item.yml` docs and tests

--- olist/models/marts/dim_date.sql
+++ olist/models/marts/dim_date.sql
@@ -4,7 +4,7 @@ with date_spine as (
     {{ dbt_utils.date_spine(
         datepart = "day",
         start_date = "cast('2016-01-01' as date)",
-        end_date= " cast('2019-01-01' as date)"
+        end_date= " cast('2021-01-01' as date)"
     ) }}
 ),
 
